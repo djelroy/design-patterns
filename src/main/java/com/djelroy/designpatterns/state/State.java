93d3e85275a4490b1438a2bf7588db8a5b78d1d4
DJ Elroy <djelroys@gmail.com>
2019-03-30 21:46:37 -0400

    Add the abstract State

---
+++ src/main/java/com/djelroy/designpatterns/state/State.java
@@ -0,0 +1,38 @@
+package com.djelroy.designpatterns.state;
+
+public abstract class State {
+
+	protected final EverydayLife life;
+	
+	public State(EverydayLife life) {
+		this.life = life;
+	}
+
+	public void wakeUp() {
+		System.out.println("You are waking up.");
+		life.setCurrentState(life.getWakingState());
+	}
+	public void sleep() {
+		System.out.println("You must be awake to fall asleep.");
+	};
+	
+	public void deepSleep() {
+		System.out.println("You must be dreaming to deep sleep.");		
+	}
+	
+	public void meditate() {
+		System.out.println("You are meditating.");
+		life.setCurrentState(life.getSamadhi());
+	};
+	
+	public void stayAware() {
+		System.out.println("Your awareness get a hold on turya");
+		life.setCurrentState(life.getTuryaGapSate());
+	}
+	
+	public void bestowBliss() {
+		System.out.println("You have to be in the supreme state of turya!");
+	}	
+	
+	public abstract void print();
+}
